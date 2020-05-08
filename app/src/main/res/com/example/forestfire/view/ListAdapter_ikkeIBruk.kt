5387785599c636db197452fe843c07cdd276df79
sofiemj <sofiemj@student.matnat.uio.no>
2020-05-08 23:22:41 +0200

    Laget et recyclerview hvor favorittene legges. Jeg har ikke fylt cardviewene med noe, så de er per nå tomme. Vi kan fylle dem på samme måte som vi gjorde i alpakka-obligen. Gjett hvor inspirasjonen min er fra

---
+++ app/src/main/res/com/example/forestfire/view/ListAdapter_ikkeIBruk.kt
@@ -0,0 +1,38 @@
+package com.example.forestfire.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.forestfire.R
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.element.view.*
+
+class ListAdapter_ikkeIBruk(private var favorites: MutableList<LatLng>) :
+    RecyclerView.Adapter<ListAdapter.ViewHolder>()  {
+
+    private var items = favorites
+
+
+    class ViewHolder constructor(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView){
+        val valgtSted = itemView.valgtSted
+
+        fun bind(latlng: LatLng){
+            valgtSted.text = latlng.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.element, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+}
