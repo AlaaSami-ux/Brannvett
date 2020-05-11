1bf6f4c0ac0e4aab50d47dea8a2f86fdffd88486
Ding Chen <dingc@student.matnat.uio.no>
2020-05-11 16:22:20 +0200

    endret størrelse til "Bål, Engangsgrill, osv."; Endret størrelse til tekster i info boksene, og lagt til mellomrom mellom avsnittene.

--- app/src/main/java/com/example/forestfire/viewModel/InfoViewModel.kt
+++ app/src/main/java/com/example/forestfire/viewModel/InfoViewModel.kt
@@ -16,76 +16,94 @@ class InfoViewModel : ViewModel() {
         val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
         builder.setIcon(R.drawable.ic_baal)
         builder.setTitle("Bål")
-        builder.setMessage("I perioden 15.april til 15. september er det forbud mot å tenne bål," +
+        builder.setMessage("\nI perioden 15.april til 15. september er det forbud mot å tenne bål," +
                 " grill og engangsgrill i skog eller i nærheten av skog. " +
-                "Når du tenner bål er det ALLTID viktig å slukke skikkelig, " +
-                "og forsikre seg om at det er helt slukket før du drar videre. \n")
+                "\n\nNår du tenner bål er det ALLTID viktig å slukke skikkelig, " +
+                "og forsikre seg om at det er helt slukket før du drar videre." +
+                "\n\n Kilde: norskfriluftsliv.no")
         builder.setPositiveButton("OK",{ dialogInterface: DialogInterface, i: Int -> })
         val dialog =builder.create()
         dialog.show()
         val msgTxt = dialog.findViewById<View>(android.R.id.message)!! as TextView
-        msgTxt.setTextSize(20F)
+        msgTxt.setTextSize(22F)
     }
 
     fun glassDialog(context: Context){
         val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
         builder.setIcon(R.drawable.ic_glassflasker)
         builder.setTitle("Glassflasker")
-        builder.setMessage("En knust glassflaske kan med hjelp av solen potensielt starte en gressbrann, " +
-                "som kan utvikle seg til en skogbrann. Ta alltid med deg søppelet ditt hjem igjen!\n")
+        builder.setMessage("\nEn knust glassflaske kan med hjelp av solen potensielt starte en gressbrann, " +
+                "som kan utvikle seg til en skogbrann. \n\nTa alltid med deg søppelet ditt hjem igjen!\n")
         builder.setPositiveButton("OK",{ dialogInterface: DialogInterface, i: Int -> })
         val dialog =builder.create()
         dialog.show()
+        val msgTxt = dialog.findViewById<View>(android.R.id.message)!! as TextView
+        msgTxt.setTextSize(22F)
       //  dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFCCD1EF")))
     }
+
     fun engangsgrillDialog(context: Context){
         val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
         builder.setIcon(R.drawable.ic_engangsgrill)
         builder.setTitle("Engangsgrill")
-        builder.setMessage("En engangsgrill skal alltid stå stødig på et underlag som ikke er brennbart, " +
+        builder.setMessage("\nEn engangsgrill skal alltid stå stødig på et underlag som ikke er brennbart, " +
                 "og husk alltid å bruke stativet som følger med. " +
-                "Pass på at grillen er ordentlig slukket og at du kaster den " +
-                "i en container spesifiser for engangsgriller. \n")
+                "\n\nPass på at grillen er ordentlig slukket og at du kaster den " +
+                "i en container spesifiser for engangsgriller." +
+                "\n\nKilde: Brannvernforeningen.no")
         builder.setPositiveButton("OK",{ dialogInterface: DialogInterface, i: Int -> })
         val dialog =builder.create()
         dialog.show()
+        val msgTxt = dialog.findViewById<View>(android.R.id.message)!! as TextView
+        msgTxt.setTextSize(22F)
        // dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFCCD1EF")))
     }
+
     fun lynDialog(context: Context){
         val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
         builder.setIcon(R.drawable.ic_lyn)
         builder.setTitle("Lyn")
-        builder.setMessage("Visste du at lynnedslag starter mange branner i skog i Norge i løpet av året? " +
+        builder.setMessage("\nVisste du at lynnedslag starter mange branner i skog i Norge i løpet av året? " +
                 "I tillegg kan det ligge og ulme i bakken i opptil tre dager før det antenner. " +
-                "Alltid meld ifra hvis du ser antydninger til skogbrann!\n")
+                "\n\nAlltid meld ifra hvis du ser antydninger til skogbrann!\n")
         builder.setPositiveButton("OK",{ dialogInterface: DialogInterface, i: Int -> })
         val dialog =builder.create()
         dialog.show()
+        val msgTxt = dialog.findViewById<View>(android.R.id.message)!! as TextView
+        msgTxt.setTextSize(22F)
         //dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFCCD1EF")))
     }
+
     fun soppelDialog(context: Context){
         val builder = AlertDialog.Builder(context,R.style.CustomAlertDialog)
         builder.setIcon(R.drawable.ic_soppelcontainer)
         builder.setTitle("Avfallscontainere")
-        builder.setMessage("Bruk alltid en avfallscontainer som er for engangsgrill etter du er ferdig å bruke den. " +
+        builder.setMessage("\nBruk alltid en avfallscontainer som er for engangsgrill etter du er ferdig å bruke den. " +
                 "Ofte finnes det en oversikt over hvilke parker eller området som har disse på kommunen sine nettsider. " +
-                "Tenk før du kaster!")
+                "\n\nTenk før du kaster!")
         builder.setPositiveButton("OK",{ dialogInterface: DialogInterface, i: Int -> })
         val dialog =builder.create()
         dialog.show()
+        val msgTxt = dialog.findViewById<View>(android.R.id.message)!! as TextView
+        msgTxt.setTextSize(22F)
        // dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFCCD1EF")))
     }
+
     fun verktoyDialog(context: Context){
         val builder = AlertDialog.Builder(context, R.style.CustomAlertDialog)
         builder.setIcon(R.drawable.ic_verktoy)
         builder.setTitle("Verktøy")
-        builder.setMessage("Bruk av verktøy som har åpen flamme," +
+        builder.setMessage("\nBruk av verktøy som har åpen flamme," +
                 " eller som slår gnister kan være svært brannfarlig. " +
                 "Det er viktig å være obs på dette, og ta de forholdsreglene som trengs. " +
-                "Gjør alltid en vurdering, og utsett heller arbeidet hvis det er fare for skogbrann.\n")
+                "\n\nGjør alltid en vurdering, og utsett heller arbeidet hvis det er fare for skogbrann." +
+                "\n\n Kilde: bondelaget.no")
+
         builder.setPositiveButton("OK",{ dialogInterface: DialogInterface, i: Int -> })
         val dialog =builder.create()
         dialog.show()
+        val msgTxt = dialog.findViewById<View>(android.R.id.message)!! as TextView
+        msgTxt.setTextSize(22F)
         //dialog.window?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFCCD1EF")))
     }
 }
