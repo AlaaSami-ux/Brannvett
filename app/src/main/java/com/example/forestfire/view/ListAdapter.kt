cb6cad4f5fec32204c027a443dbce73f6872cfdf
sofiemj <sofiemj@student.matnat.uio.no>
2020-05-11 21:17:17 +0200

    fiksa datoene!

--- app/src/main/java/com/example/forestfire/view/ListAdapter.kt
+++ app/src/main/java/com/example/forestfire/view/ListAdapter.kt
@@ -10,12 +10,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.forestfire.R
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.element.view.*
+import java.util.*
 
 class ListAdapter(private var favorites: MutableMap<LatLng, String>) :
     RecyclerView.Adapter<ListAdapter.ViewHolder>()  {
 
     private var positions = favorites.keys // MutableSet of keys from favorites map
     private var places = favorites.values // MutableCollection of values from favorites map
+
+    val c = Calendar.getInstance()
 
     class ViewHolder constructor(
         itemView: View
@@ -24,15 +27,22 @@ class ListAdapter(private var favorites: MutableMap<LatLng, String>) :
         val dag1: TextView = itemView.dag1
         val vær1: TextView = itemView.dag1
         val brannfare1: TextView = itemView.dag1
-        val dag2: TextView = itemView.dag1
-        val vær2: TextView = itemView.dag1
-        val brannfare2: TextView = itemView.dag1
-        val dag3: TextView = itemView.dag1
-        val vær3: TextView = itemView.dag1
-        val brannfare3: TextView = itemView.dag1
-        fun bind(place: String){
+        val dag2: TextView = itemView.dag2
+        val vær2: TextView = itemView.dag2
+        val brannfare2: TextView = itemView.dag2
+        val dag3: TextView = itemView.dag3
+        val vær3: TextView = itemView.dag3
+        val brannfare3: TextView = itemView.dag3
+        fun bind(place: String, c: Calendar){
             valgtSted.text = place
-            dag1.text = "noe"
+            var dato = c.get(Calendar.DAY_OF_MONTH).toString() + "/" + (c.get(Calendar.MONTH)+1).toString()
+            dag1.text = dato
+            c.roll(Calendar.DATE, 1)
+            dato = c.get(Calendar.DAY_OF_MONTH).toString() + "/" + (c.get(Calendar.MONTH)+1).toString()
+            dag2.text = dato
+            c.roll(Calendar.DATE, 1)
+            dato = c.get(Calendar.DAY_OF_MONTH).toString() + "/" + (c.get(Calendar.MONTH)+1).toString()
+            dag3.text = dato
             Log.d("ListAdapter Adresse:", place)
         }
     }
@@ -46,6 +56,6 @@ class ListAdapter(private var favorites: MutableMap<LatLng, String>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(places.elementAt(position))
+        holder.bind(places.elementAt(position), c)
     }
 }
