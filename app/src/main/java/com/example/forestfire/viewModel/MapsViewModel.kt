2a403db95d118e17a0053b0524314d09b430b545
sofiemj <sofiemj@student.matnat.uio.no>
2020-05-20 21:39:38 +0200

    fikset min posisjon bug

--- app/src/main/java/com/example/forestfire/viewModel/MapsViewModel.kt
+++ app/src/main/java/com/example/forestfire/viewModel/MapsViewModel.kt
@@ -6,10 +6,14 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.media.Image
 import android.util.Log
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
+import com.example.forestfire.R
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -27,7 +31,6 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
     private var MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1
     private var MY_PERMISSIONS_REQUEST_COARSE_LOCATION = 1
 
-    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
     private var mLocationPermissionGranted = false // assume location permission is not granted
 
     private lateinit var marker: Marker
@@ -37,15 +40,18 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
     private val oslo = LatLng(59.911491, 10.757933)
     private var lastUsedLocation: LatLng = oslo
     private var lastUsedLocationName: String = "Oslo"
-    private var deviceLoc: LatLng = oslo
+    private var deviceLoc: LatLng = lastUsedLocation
     private var merInfoVises: Boolean = false
 
     private lateinit var activity: Activity
     private lateinit var context: Context
+    private lateinit var mMap: GoogleMap
+    private lateinit var sted1: TextView
+    private lateinit var sted2: TextView
+    private lateinit var favBtn1: ImageButton
+    private lateinit var favBtn2: ImageButton
+    private var favorites: MutableList<LatLng> = ArrayList()
 
-    fun setFusedLocationProviderClient(){
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
-    }
 
     fun setActivity(a: Activity){
         activity = a
@@ -53,39 +59,25 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
 
     fun setContext(c: Context){
         context = c
-        mFusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(context)
     }
 
-    /*fun getDeviceLocation(mMap: GoogleMap): LatLng {
-        Log.d(TAG, "getDeviceLocation called")
-        findDeviceLocation(mMap)
-        Log.d(TAG, "After findDeviceLocation called")
-        return deviceLoc
+    fun setMap(mMap: GoogleMap){
+        this.mMap = mMap
     }
 
-    fun findDeviceLocation(mMap: GoogleMap){
-        Log.d(TAG, "findDeviceLocation called")
-        mFusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
-            if (location != null) {
-                Log.d(TAG, "found a location")
-                deviceLoc = LatLng(location.latitude, location.longitude)
-                if (!norge.contains(deviceLoc)){ // enheten befinner seg ikke i norge
-                    Log.d(TAG, "location outside of Norway")
-                    Toast.makeText(context,
-                        "Din posisjon er utenfor Norge. Søk på et sted i Norge",
-                        Toast.LENGTH_LONG).show()
-                } else {
-                    Log.d(TAG, "location in Norway")
-                    deviceLoc = LatLng(location.latitude, location.longitude)
-                }
-            } else {
-                moveCam(mMap, oslo)
-                Toast.makeText(context, "kunne ikke finne posisjonen din", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
-    }*/
+    fun setTextViews(v1: TextView, v2: TextView){
+        sted1 = v1
+        sted2 = v2
+    }
+
+    fun setFavButtons(b1: ImageButton, b2: ImageButton){
+        favBtn1 =  b1
+        favBtn2 =  b2
+    }
+
+    fun setCurrentFavorites(favs: MutableList<LatLng>){
+        favorites = favs
+    }
 
     fun getLocationPermission(){
         // get permission to get current location
@@ -119,30 +111,47 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
             )
         }
     }
-
-    fun getUserLocation(): LatLng?{
-        Log.d(TAG, "getUserLocation called")
-        var sted: LatLng? = null
+/*
+    fun getLastDeviceLocation(){
+        // finner siste posisjon funnet for enheten
+        Log.d(TAG, "getLastDeviceLocation")
         mFusedLocationProviderClient.lastLocation.addOnSuccessListener {
-            // kan bli null
-            if (it != null){
-                sted = LatLng(it.latitude, it.longitude)
-                if (!norge.contains(sted)){
-                    sted = null
+            // fikk tak i sist kjente lokasjon
+            if(it != null && norge.contains(LatLng(it.latitude, it.longitude))){
+                Log.d(TAG, "found a device location: " + LatLng(it.latitude, it.longitude).toString())
+                deviceLoc = LatLng(it.latitude, it.longitude)
+                // fant en lokasjon. Flytter kartet dit
+                Log.d(TAG, "flytter kamera og legger på markør")
+                moveCam(deviceLoc)
+                addMarker(deviceLoc)
+                // oppdaterer sist brukte posisjon
+                Log.d(TAG, "oppdaterer sist brukte posisjon til $deviceLoc")
+                setLastUsedLocation(deviceLoc)
+                Log.d(TAG, "oppdaterer stedsnavn")
+                sted1.text = getAddressFromLocation(deviceLoc.latitude, deviceLoc.longitude)
+                sted2.text = sted1.text
+                if (favorites.contains(deviceLoc)){
+                    Log.d(TAG, "the device location is already a favorite")
+                    favBtn1.setBackgroundResource(R.drawable.ic_favorite_black_24dp)
+                    favBtn2.setBackgroundResource(R.drawable.ic_favorite_black_24dp)
+                } else {
+                    favBtn1.setBackgroundResource(R.drawable.ic_favorite_border_black_24dp)
+                    favBtn2.setBackgroundResource(R.drawable.ic_favorite_border_black_24dp)
+                    Log.d(TAG, "the device location is not a favorite")
                 }
             }
         }
-        return sted
     }
+ */
 
-    fun moveCam(mMap: GoogleMap, ll: LatLng) {
+    fun moveCam(ll: LatLng) {
         //lastUsedLocation = ll
         Log.d(TAG, "moveCam to $ll")
         mMap.setLatLngBoundsForCameraTarget(norge)
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, DEFAULT_ZOOM))
     }
 
-    fun addMarker(mMap: GoogleMap, ll: LatLng) {
+    fun addMarker(ll: LatLng) {
         Log.d(TAG, "add marker to $ll")
         if (::marker.isInitialized) {
             marker.remove()
@@ -161,7 +170,6 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
                 val strAddress: String = fetchedAddress.getAddressLine(0)
                 sted = strAddress.split(",", ignoreCase=true, limit=0).first()
                 Log.d(TAG, "sted: $sted")
-            } else {
             }
         } catch (e: IOException) {
             e.printStackTrace()
