89cdaf024b18f57c37180e1d471aaf5802935a94
sofiemj <sofiemj@student.matnat.uio.no>
2020-05-21 11:03:33 +0200

    fikset bugs med favoritter og kart

--- app/src/main/java/com/example/forestfire/viewModel/MapsViewModel.kt
+++ app/src/main/java/com/example/forestfire/viewModel/MapsViewModel.kt
@@ -6,16 +6,10 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
-import android.media.Image
 import android.util.Log
-import android.widget.ImageButton
-import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
-import com.example.forestfire.R
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
@@ -37,20 +31,14 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
     private val norge = LatLngBounds(
         LatLng(58.019156, 2.141567), LatLng(71.399348, 33.442113)
     )
-    private val oslo = LatLng(59.911491, 10.757933)
+    private val oslo = LatLng(59.9138688,10.7522454)
     private var lastUsedLocation: LatLng = oslo
     private var lastUsedLocationName: String = "Oslo"
-    private var deviceLoc: LatLng = lastUsedLocation
     private var merInfoVises: Boolean = false
 
     private lateinit var activity: Activity
     private lateinit var context: Context
     private lateinit var mMap: GoogleMap
-    private lateinit var sted1: TextView
-    private lateinit var sted2: TextView
-    private lateinit var favBtn1: ImageButton
-    private lateinit var favBtn2: ImageButton
-    private var favorites: MutableList<LatLng> = ArrayList()
 
 
     fun setActivity(a: Activity){
@@ -63,20 +51,6 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
 
     fun setMap(mMap: GoogleMap){
         this.mMap = mMap
-    }
-
-    fun setTextViews(v1: TextView, v2: TextView){
-        sted1 = v1
-        sted2 = v2
-    }
-
-    fun setFavButtons(b1: ImageButton, b2: ImageButton){
-        favBtn1 =  b1
-        favBtn2 =  b2
-    }
-
-    fun setCurrentFavorites(favs: MutableList<LatLng>){
-        favorites = favs
     }
 
     fun getLocationPermission(){
@@ -111,38 +85,6 @@ class MapsViewModel(): ViewModel(){ //AndroidViewModel(app)
             )
         }
     }
-/*
-    fun getLastDeviceLocation(){
-        // finner siste posisjon funnet for enheten
-        Log.d(TAG, "getLastDeviceLocation")
-        mFusedLocationProviderClient.lastLocation.addOnSuccessListener {
-            // fikk tak i sist kjente lokasjon
-            if(it != null && norge.contains(LatLng(it.latitude, it.longitude))){
-                Log.d(TAG, "found a device location: " + LatLng(it.latitude, it.longitude).toString())
-                deviceLoc = LatLng(it.latitude, it.longitude)
-                // fant en lokasjon. Flytter kartet dit
-                Log.d(TAG, "flytter kamera og legger på markør")
-                moveCam(deviceLoc)
-                addMarker(deviceLoc)
-                // oppdaterer sist brukte posisjon
-                Log.d(TAG, "oppdaterer sist brukte posisjon til $deviceLoc")
-                setLastUsedLocation(deviceLoc)
-                Log.d(TAG, "oppdaterer stedsnavn")
-                sted1.text = getAddressFromLocation(deviceLoc.latitude, deviceLoc.longitude)
-                sted2.text = sted1.text
-                if (favorites.contains(deviceLoc)){
-                    Log.d(TAG, "the device location is already a favorite")
-                    favBtn1.setBackgroundResource(R.drawable.ic_favorite_black_24dp)
-                    favBtn2.setBackgroundResource(R.drawable.ic_favorite_black_24dp)
-                } else {
-                    favBtn1.setBackgroundResource(R.drawable.ic_favorite_border_black_24dp)
-                    favBtn2.setBackgroundResource(R.drawable.ic_favorite_border_black_24dp)
-                    Log.d(TAG, "the device location is not a favorite")
-                }
-            }
-        }
-    }
- */
 
     fun moveCam(ll: LatLng) {
         //lastUsedLocation = ll
